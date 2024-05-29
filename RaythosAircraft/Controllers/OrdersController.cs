3b0fc41dd13f47af0ea420231bcc9b06f952e0b3
sugikaran <133148272+sugikaran@users.noreply.github.com>
2024-05-29 16:29:28 +0530

    Add project files.

---
+++ RaythosAircraft/Controllers/OrdersController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RaythosAircraft.Data;
+using RaythosAircraft.Models;
+
+namespace RaythosAircraft.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class OrdersController : Controller
+    {
+        private readonly RaythosAircraft_db_Context _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public OrdersController(RaythosAircraft_db_Context context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        // GET: Orders
+        public async Task<IActionResult> Index(string SearchBy, string search)
+        {
+            if (SearchBy == "OrderId" && int.TryParse(search, out var orderId))
+            {
+                return View(await _context.Order.Where(x => x.OrderId == orderId || search == null).ToListAsync());
+            }
+            else
+            {
+                return View(await _context.Order.Where(x => x.Name.StartsWith(search) || search == null).ToListAsync());
+            }
+
+            // Add a default return statement (e.g., redirect or default view)
+            return RedirectToAction("Index", "Orders"); // Replace with your desired action and controller
+        }
+
+
+
+
+        // GET: Orders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Order == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Order
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // GET: Orders/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Orders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("OrderId,Name,Email,Address,ContactNo,AgreementStatus,PurchaseDate,DeliveryDate,Days,Status,Total,ProductCode,ItemCode")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Order == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Order.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("OrderId,Name,Email,Address,ContactNo,AgreementStatus,PurchaseDate,DeliveryDate,Days,Status,Total,ProductCode,ItemCode")] Order order)
+        {
+            if (id != order.OrderId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.OrderId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(order);
+        }
+
+        // GET: Orders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Order == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Order
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Order == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Order'  is null.");
+            }
+            var order = await _context.Order.FindAsync(id);
+            if (order != null)
+            {
+                _context.Order.Remove(order);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Order.Any(e => e.OrderId == id);
+        }
+    }
+}
---
+++ RaythosAircraft/Models/Order.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RaythosAircraft.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string ContactNo { get; set; }
+
+        public string AgreementStatus { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+
+        public DateTime DeliveryDate { get; set; }
+
+        public string Status { get; set; }
+
+        public double Total {  get; set; }
+
+
+        //Foreign Keys
+        public string ProductCode { get; set; }
+
+        public string ItemCode { get; set; }
+    }
+}
